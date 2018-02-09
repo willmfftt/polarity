13f50229646ede9802d494978905df4a95110087
William Moffitt <wmoffitt@cybrtalk.com>
2018-02-08 20:54:07 -0500

    Added utils directory and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='polarity',
+    version='0.0.1',
+    packages=[],
+    url='',
+    license='',
+    author='William Moffitt',
+    author_email='wmoffitt@cybrtalk.com',
+    description=''
+)
